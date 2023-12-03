b3f671ee6b7e2048987deadb2303b7f76147673c
AronMav <codymaverick@bk.ru>
2023-12-03 14:10:09 +0400

    feat: 2.5 Adding Binary Flags

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -5,14 +5,20 @@ struct Options {
     #[clap(default_value = "Meow!")]
     /// What does the cat say?
     message: String,
+    #[clap(short = 'd', long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
 }
 fn main() {
     let options = Options::parse();
     let message = options.message;
+
+    let eye = if options.dead { "x" } else { "o" };
+
     println!("{}", message);
     println!(" \\");
     println!("  \\");
     println!("     /\\_/\\");
-    println!("    ( o o )");
+    println!("    ( {eye} {eye} )");
     println!("    =( I )=");
 }
