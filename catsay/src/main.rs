0730cefb3d2c2b75b62d3cf06bb6f87e13af4b41
AronMav <codymaverick@bk.ru>
2023-12-03 22:03:01 +0400

    feat: 2.8 Reading the Cat Picture from a File

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,4 +1,3 @@
-
 use clap::Parser;
 use colored::Colorize;
 
@@ -10,21 +9,40 @@ struct Options {
     #[clap(short = 'd', long = "dead")]
     /// Make the cat appear dead
     dead: bool,
+    #[clap(short = 'f', long = "file")]
+    /// Load the cat picture from specified file
+    catfile: Option<std::path::PathBuf>,
 }
 fn main() {
     let options = Options::parse();
     let message = options.message;
 
-    let eye = if options.dead { "x" } else { "o" };
-
     if message.to_lowercase() == "woof" {
         eprintln!("A cat shouldn't bark like a dog.")
     };
 
-    println!("{}", message.bright_yellow().underline().on_purple());
-    println!(" \\");
-    println!("  \\");
-    println!("     /\\_/\\");
-    println!("    ( {eye} {eye} )", eye = eye.red().bold());
-    println!("    =( I )=");
+    let eye = if options.dead { "x" } else { "o" };
+
+    match &options.catfile {
+        Some(path) => {
+            let cat_template = std::fs::read_to_string(path)
+                .expect(&format!("could not read file {:?}", path));
+            let eye = format!("{}", eye.red().bold());
+            let cat_picture = cat_template.replace("{eye}", &eye);
+            println!(
+                "{}",
+                message.bright_yellow().underline().on_purple()
+            );
+            println!("{}", &cat_picture);
+        },
+        None => {
+
+            println!("{}", message.bright_yellow().underline().on_purple());
+            println!(" \\");
+            println!("  \\");
+            println!("     /\\_/\\");
+            println!("    ( {eye} {eye} )", eye = eye.red().bold());
+            println!("    =( I )=");
+        }
+    }
 }
