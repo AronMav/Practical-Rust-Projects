a14736a5f71b582cf7b3a02025e548891fd9b8da
AronMav <codymaverick@bk.ru>
2023-12-03 14:16:05 +0400

    feat: 2.6 Printing to STDERR

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -15,6 +15,10 @@ fn main() {
 
     let eye = if options.dead { "x" } else { "o" };
 
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark like a dog.")
+    };
+
     println!("{}", message);
     println!(" \\");
     println!("  \\");
