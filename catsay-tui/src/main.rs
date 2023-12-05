173bf6ba3373dcdd5e8eeaa5de1d63189edefd00
AronMav <codymaverick@bk.ru>
2023-12-06 00:20:28 +0400

    feat: 3.4 Handling Simple Keyboard Inputs

--- catsay-tui/src/main.rs
+++ catsay-tui/src/main.rs
@@ -1,4 +1,6 @@
-use cursive::views::TextView;
+use cursive::{
+    views::TextView,
+    event::Key};
 
 fn main() {
     let mut siv = cursive::default();
@@ -9,7 +11,11 @@ fn main() {
     /\\_/\\
    ( o o )
    =( I )=";
-    // Declaring the app layout
+    // Declaring the app layer
     siv.add_layer(TextView::new(cat_text));
+
+    // Listen to Key::Esk and quit
+    siv.add_global_callback(Key::Esc, |s| s.quit());
+
     siv.run();
 }
