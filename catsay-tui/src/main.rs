5c5071f19e0ed5df03c8f8d24c10d5cc70e22508
AronMav <codymaverick@bk.ru>
2023-12-05 23:51:23 +0400

    feat: 3.3 Showing a Dialog Box

--- catsay-tui/src/main.rs
+++ catsay-tui/src/main.rs
@@ -1,5 +1,15 @@
+use cursive::views::TextView;
+
 fn main() {
     let mut siv = cursive::default();
 
-    siv.run(); // starting the event loop
+    let cat_text = "Meow!
+\\
+  \\
+    /\\_/\\
+   ( o o )
+   =( I )=";
+    // Declaring the app layout
+    siv.add_layer(TextView::new(cat_text));
+    siv.run();
 }
