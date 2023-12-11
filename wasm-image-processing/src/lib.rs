bbb4786958c7cf92ba351caca0ad581caeb98720
AronMav <codymaverick@bk.ru>
2023-12-11 23:11:33 +0400

    feat: 4.4 Resizing an Image with WebAssembly. Passing the Image to Wasm

--- wasm-image-processing/src/lib.rs
+++ wasm-image-processing/src/lib.rs
@@ -1,13 +1,29 @@
-mod utils;
-
+use image::{RgbaImage};
+use image::imageops;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+// When the 'wee_alloc' feature is enabled, use 'wee_alloc'
+// as the global allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-image-processing!");
+pub fn shrink_by_half(
+    original_image: Vec<u8>,
+    width: u32,
+    height: u32
+) -> Vec<u8> {
+    let image: RgbaImage =
+        image::ImageBuffer::from_vec(
+            width,height,original_image
+        ).unwrap();
+    let output_image = imageops::resize(
+        &image,
+        width/2,
+        height/2,
+        imageops::FilterType::Nearest
+    );
+
+    output_image.into_vec()
 }
