7bbdba912a3f6d8a7767b299c413d09392ad5b11
AronMav <codymaverick@bk.ru>
2023-12-17 13:52:43 +0400

    feat: 4.7 A Cat Management Application. Deleting Files

--- catdex-yew/src/main.rs
+++ catdex-yew/src/main.rs
@@ -36,18 +36,30 @@ fn app() -> Html {
         }
     };
 
+    let delete_cat = {
+        let cat_list = cat_list.clone();
+        Callback::from(move |name: String|{
+            let interior_cat_list = cat_list.deref().clone();
+            let new_cat_list: Vec<_> = interior_cat_list
+                .into_iter()
+                .filter(|cat| cat.name != name).collect();
+            cat_list.set(new_cat_list);
+        })
+    };
+
     html! {
         <div class= {stylesheet}>
             <h1>{format!("Catdex")}</h1>
             <label for="file-upload"/>
             <input
-                id="file-upload"
                 type="file"
                 accept="image/*"
                 onchange={on_change}
             />
             <section class="cats">
-                { for cat_list.iter().map(cat) }
+                { for cat_list.iter().map(
+                    |val| cat(val, delete_cat.clone())
+                )}
             </section>
         </div>
     }
@@ -72,17 +84,47 @@ struct CatDetails {
     image: Vec<u8>,
 }
 
-fn cat(cat: &CatDetails) -> Html {
+fn cat(cat: &CatDetails, callback: Callback<String>) -> Html {
     html! {
         <article class="cat">
             <h3>{ format!( "{}", cat.name)}</h3>
-            <img src ={
-                format!("data:image;base64,{}",
+            <Button text={"Delete".to_string()}
+                name = {cat.name.clone()}
+                on_click ={callback}
+            />
+            <img src ={format!(
+                "data:image;base64,{}",
                 general_purpose::STANDARD.encode(&cat.image))
             } />
         </article>
     }
 }
+
+#[derive(Properties, PartialEq)]
+struct ButtonProp {
+    text: String,
+    name: String,
+    on_click: Callback<String>
+}
+
+#[function_component(Button)]
+fn delete_button(button: &ButtonProp) -> Html {
+    let on_click = {
+        let name = button.name.clone();
+        let callback = button.on_click.clone();
+        move |_| {
+            callback.emit(name.clone())
+        }
+    };
+    html! {
+        <div>
+            <button onclick = {on_click}>
+                {{button.text.clone()}}
+            </button>
+        </div>
+    }
+}
+
 
 fn main() {
     yew::Renderer::<App>::new().render();
