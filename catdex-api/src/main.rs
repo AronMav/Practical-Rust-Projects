ee69c67dc37ee44dd8efc59c585c7efc8001db29
AronMav <codymaverick@bk.ru>
2023-12-20 22:45:39 +0400

    feat: 5.4 Converting the Cats List to a REST API

--- catdex-api/src/main.rs
+++ catdex-api/src/main.rs
@@ -1,8 +1,37 @@
-use actix_web::{web, App, HttpServer, Result};
+use actix_web::{web, App, HttpServer, Result, Responder};
 use actix_files::{Files, NamedFile};
+use serde::Serialize;
 
 async fn index() -> Result<NamedFile> {
     Ok(NamedFile::open("./static/index.html")?)
+}
+
+#[derive(Serialize)]
+pub struct Cat{
+    pub id: i32,
+    pub name: String,
+    pub image_path: String,
+}
+
+async fn cats_endpoint() -> impl Responder {
+    let cats = vec![
+        Cat{
+            id: 1,
+            name: "British Short Hair".to_string(),
+            image_path: "image/british-short-hair.jpg".to_string(),
+        },
+        Cat{
+            id: 2,
+            name: "Persian".to_string(),
+            image_path: "image/persian.jpg".to_string(),
+        },
+        Cat{
+            id: 3,
+            name: "Ragdoll".to_string(),
+            image_path: "image/ragdoll.jpg".to_string(),
+        },
+    ];
+    return web::Json(cats);
 }
 
 #[actix_web::main]
@@ -17,6 +46,10 @@ async fn main() -> std::io::Result<()> {
             .service(
                 Files::new("/image","image")
                     .show_files_listing(),
+            )
+            .service(
+                web::scope("/api")
+                    .route("/cats", web::get().to(cats_endpoint)),
             )
             .route("/", web::get().to(index))
     })
